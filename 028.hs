69f1516a1b38e78d1371ddcef30b72b02281f2ec
Michael Ficarra <git@michael.ficarra.me>
2012-02-09 02:41:35 -0500

    list comprehensions save the day once again

--- 028.hs
+++ 028.hs
@@ -3,4 +3,4 @@ numDiagonals n = foldl (\a b -> a + 4) 1 [3,5..n]
 sum $ take (numDiagonals 1001) diagonals
 
 -- or, after some analysis, simply:
-(+ 1) $ sum $ map (\n -> 4 * (n ^ 2) - (6 * n) + 6) [3,5..1001]
+1 + sum [4 * (n ^ 2) - (6 * n) + 6 | n <- [3,5..1001]]
